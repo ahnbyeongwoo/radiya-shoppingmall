d17935ab937daa8709b4e54945b4d280a688d155
ahnbyeongwoo <bw5871@gmail.com>
2025-05-29 17:51:34 +0900

    env file, config.js file

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ module.exports = defineConfig({
   devServer:{
     proxy:{
       '/api':{
-        target:`${process.env.VUE_APP_API_URL}`,
+        target:'https://radiya-shoppingmall-1.onrender.com',
         changeOrigin:true,
         pathRewrite:{
           '^/api':''
